71fce82f21397644b0df02145701c7b4a0c394e9
Spandan <74659727+Spandan-maker@users.noreply.github.com>
2023-06-04 16:32:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import {Text, View, StyleSheet} from 'react-native'
+import Home from './screens/home'
+
+export default class App extends Component{
+  render(){
+    return(
+      <Home/>
+    )
+  }
+}
